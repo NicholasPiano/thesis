21e213e6ec322a2a357ccdbf2d238c7ba5cfc529
NicholasPiano <nicholas.d.piano@gmail.com>
2015-08-18 13:13:54 +0100

    word count script and progress file

--- main/main.py
+++ main/main.py
@@ -1,1 +1,55 @@
 # word counts and tree structure
+
+import os
+from os.path import join, isdir
+
+def word_count(file_path):
+  count = 0
+  with open(file_path) as f:
+    for line in f.readlines():
+      count += len(line.split(' '))
+
+  return count
+
+root_path = '/Users/nicholaspiano/Documents/Employment/Cambridge/thesis/main'
+progress_path = join(root_path, 'progress.txt')
+
+# for each folder in root directory
+with open(progress_path, 'w+') as progress_file:
+  total_words = 0
+  for section in [s for s in os.listdir(root_path) if ('.DS' not in s and isdir(join(root_path, s)))]:
+
+    section_path = join(root_path, section)
+    section_total_words = 0
+    progress_file.write('{}\n'.format(section))
+
+    for subsection in [s for s in os.listdir(section_path) if ('.DS' not in s)]:
+
+      subsection_path = join(section_path, subsection)
+      subsection_total_words = 0
+
+      if isdir(subsection_path):
+        progress_file.write('-- {}\n'.format(subsection))
+        for subsubsection in [s for s in os.listdir(subsection_path) if ('.DS' not in s)]:
+
+          subsubsection_path = join(subsection_path, subsubsection)
+          subsubsection_total_words = word_count(subsubsection_path)
+          progress_file.write('-- -- {} words -- {}\n'.format(subsubsection_total_words, subsubsection))
+          subsection_total_words += subsubsection_total_words
+
+        progress_file.write('-- {} words\n'.format(subsection_total_words))
+        progress_file.write('\n')
+
+      else:
+        progress_file.write('-- {} words -- {}\n'.format(word_count(subsection_path), subsection))
+        progress_file.write('\n')
+
+      section_total_words += subsection_total_words
+
+    total_words += section_total_words
+
+    progress_file.write('{} words\n'.format(section_total_words))
+    progress_file.write('\n')
+    progress_file.write('\n')
+
+  progress_file.write('{} words\n'.format(total_words))
